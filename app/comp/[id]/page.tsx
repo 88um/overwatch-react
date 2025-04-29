5cc39a4e0898b7d270e2d386f248022c00cf7ec4
88um <atriciaurray@gmail.com>
2025-04-29 13:48:13 -0500

    add comp h2

--- app/comp/[id]/page.tsx
+++ app/comp/[id]/page.tsx
@@ -42,6 +42,9 @@ const CompPage: React.FC<CompPageProps> = async ({ params }) => {
         <TeamModal/>
       </div>
       <div>
+        <h2 className="text-2xl font-bold mb-4">Your Comp - {tank.comp}</h2>
+      </div>
+      <div>
       <h2 className="text-2xl font-bold mb-4">For Map</h2>
       <MapCard map={map} />
       </div>
